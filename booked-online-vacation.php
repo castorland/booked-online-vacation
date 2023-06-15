474e1c6e2548c38625d8989b1dc5c6390d4a6632
Hódos Gábor <gabor.hodos@webdeveloping.hu>
2023-06-15 07:47:20 +0200

    Frontend functions

--- booked-online-vacation.php
+++ booked-online-vacation.php
@@ -10,7 +10,6 @@
 
 // BACKEND
 
-//include CSS and JS
 function booked_online_vacation_enqueue_assets () {
     wp_enqueue_style( 'booked-online-vacation-styles', plugin_dir_url( __FILE__ ) . 'css/booked-online-admin.css' );
     wp_enqueue_script( 'booked-online-vacation-scripts', plugin_dir_url( __FILE__ ) . 'js/booked-online-admin.js', array('jquery') );
@@ -33,13 +32,13 @@ function booked_online_vacation( $day, $time, $calendar_id ) {
         $timeslot = implode('-', $time);
         $details = get_option('booked_defaults_'.$calendar_id);
         $onlineVacationBool = filter_var($details[$day.'-details'][$timeslot]['onlineVacationCheckbox'], FILTER_VALIDATE_BOOLEAN);
-        
+
         ?>
         <span class="onlineVacation">
             <label for="onlineVacationCheckbox">Csak utólag nézhető</label>
             <input id="onlineVacationCheckbox" name="onlineVacationCheckbox" type="checkbox" value="1" <?php if ($onlineVacationBool): echo ' checked="checked"'; endif; ?>>
             </span>
-        <?php   
+        <?php
     }
 }
 add_action( 'booked_single_timeslot_end', 'booked_online_vacation', 10, 3);
@@ -52,7 +51,7 @@ function booked_online_vacation_set () {
 		$day = $_POST['day'];
 		$timeslot = $_POST['timeslot'];
 		$onlineVacation = $_POST['onlineVacationCheckbox'];
-        
+
 		if ($calendar_id):
 			$booked_defaults = get_option('booked_defaults_'.$calendar_id);
 		else :
@@ -61,7 +60,7 @@ function booked_online_vacation_set () {
 
 		if (!empty($booked_defaults[$day][$timeslot])):
 		    $booked_defaults[$day.'-details'][$timeslot]['onlineVacationCheckbox'] = $onlineVacation;
-            
+
 			if ($calendar_id):
 				update_option('booked_defaults_'.$calendar_id,$booked_defaults);
 			else :
@@ -76,113 +75,76 @@ function booked_online_vacation_set () {
 }
 add_action('wp_ajax_booked_online_vacation_set', 'booked_online_vacation_set');
 
-add_action( 'get_header', 'my_log' );
+//add_action( 'get_header', 'my_log' );
 
 function my_log( $txt ) {
-    $file = plugin_dir_path( __FILE__ ) . '/logs.txt'; 
+    $file = plugin_dir_path( __FILE__ ) . '/logs.txt';
     $open = fopen( $file, "a" );
-    
-    $write = fwrite($open, date('Y-m-d H:i:s') . PHP_EOL);
+
+    fwrite($open, date('Y-m-d H:i:s') . PHP_EOL);
 
     if (is_array($txt) || is_object($txt)) {
-        $write = fwrite($open, print_r($txt, true));
+        fwrite($open, print_r($txt, true));
     } else {
-        $write = fwrite( $open, $txt );     
+        fwrite( $open, $txt );
     }
-    
+    fwrite( $open, PHP_EOL );
+
     fclose( $open );
 }
 
 
 // FRONTEND
 function booked_online_vacation_enqueue_front_assets(){
-    wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
     wp_enqueue_script( 'booked_online_vacation_enqueue_front_jf', plugin_dir_url( __FILE__ ) . 'js/booked-online-front.js', array('jquery'), false, true );
 }
 
 add_action('wp_enqueue_scripts', 'booked_online_vacation_enqueue_front_assets');
 
-
-/*
-function check_online_vacation( $field_type, $field_name, $field_value, $is_required, $look_for_subs, $numbers_only, $data_attributes ) {
-    my_log('check_online_vacation'.PHP_EOL);
-    my_log(compact('field_type', 'field_name', 'field_value', 'is_required', 'look_for_subs', 'numbers_only', 'data_attributes'));
-    if ($field_name == 'required---3091567' && $field_value == 'Élőben veszek részt') {
-        $data_attributes .= ' disabled';
-        my_log($data_attributes.PHP_EOL);
-        return $data_attributes;
+function booked_online_vacation_get()
+{
+    if (!wp_verify_nonce($_POST['nonce'], 'ajax-nonce')) {
+        die('Required "nonce" value is not here, please let the developer know.');
     }
+
+    if (isset($_POST['date']) && isset($_POST['timeslot']) && isset($_POST['calendar_id'])) :
+
+        $calendar_id = (isset($_POST['calendar_id']) ? $_POST['calendar_id'] : false);
+
+        $date = $_POST['date'];
+        $timeslot = $_POST['timeslot'];
+        $day = date('D', strtotime($date));
+
+        if ($calendar_id) :
+            $booked_defaults = get_option('booked_defaults_' . $calendar_id);
+        else :
+            $booked_defaults = get_option('booked_defaults');
+        endif;
+
+
+        if (!empty($booked_defaults[$day][$timeslot])) :
+            $onlineVacationBool = filter_var($booked_defaults[$day . '-details'][$timeslot]['onlineVacationCheckbox'], FILTER_VALIDATE_BOOLEAN);
+        else :
+            $booked_custom_timeslots_encoded = get_option('booked_custom_timeslots_encoded');
+            $booked_custom_timeslots_decoded = json_decode($booked_custom_timeslots_encoded, true);
+
+            foreach ($booked_custom_timeslots_decoded['booked_custom_calendar_id'] ?? [] as $key => $value) {
+                $booked_custom_start_date = $booked_custom_timeslots_decoded['booked_custom_start_date'];
+
+                if ($value == $calendar_id && $booked_custom_start_date[$key] == $date) {
+                    $booked_defaults = json_decode($booked_custom_timeslots_decoded['booked_this_custom_timelots_details'][$key], true);
+                    $onlineVacationBool = filter_var($booked_defaults[$timeslot]['customOnlineVacationCheckbox'], FILTER_VALIDATE_BOOLEAN);
+                }
+            }
+        endif;
+        wp_send_json_success(array(
+            'onlineVacationCheckbox' => $onlineVacationBool
+        ));
+    endif;
+
+    return 'fail';
+    wp_die();
 }
 
-add_filter( 'booked_custom_fields_add_template_subs', 'check_online_vacation', 10, 7 );
-*/
-
-/*
-function check_online_vacation2( $default_value, $field_type, $field_name, $field_value, $is_required, $look_for_subs, $numbers_only, $data_attributes ) {
-    my_log('check_online_vacation2'.PHP_EOL);
-    my_log(compact('default_value', 'field_type', 'field_name', 'field_value', 'is_required', 'look_for_subs', 'numbers_only', 'data_attributes'));
-    if ($field_name == 'single-radio-button---3091567' && $field_value == 'Élőben veszek részt') {
-        $data_attributes .= ' disabled';
-        
-        my_log($data_attributes.PHP_EOL);
-        return $data_attributes;
-    }
-}
-
-add_filter( 'booked_custom_fields_add_template_main', 'check_online_vacation2', 10, 8 );
-*/
-/*
-add_filter( 'booked_fe_calendar_date_appointments', 'online_vacation_date_appointments', 10, 7);
-
-function online_vacation_date_appointments($html,$time_format,$timeslot_parts,$spots_available,$available,$timeslot,$date){
-    //my_log('online_vacation_date_appointments'.PHP_EOL);
-    //my_log(compact('html', 'time_format', 'timeslot_parts', 'spots_available', 'available', 'timeslot', 'date'));
-    return $html;
-}
-*/
-
-function online_vacation_booked_fe_appt_form_bookings( $bookings ){
-    foreach ($bookings as &$booking) {
-        foreach ($booking as &$timeSlot) {
-            $details = get_option('booked_defaults_'.$timeSlot['calendar_id']);
-            $day = date('D', strtotime($timeSlot['date']));
-            $timeslot = $timeSlot['timeslot'];
-            $onlineVacationBool = filter_var($details[$day.'-details'][$timeslot]['onlineVacationCheckbox'], FILTER_VALIDATE_BOOLEAN);
-            $timeSlot['onlineVacation'] = $onlineVacationBool;
-        }
-    }
-    my_log('booked_fe_appt_form_bookings');
-    my_log($bookings);
-    return $bookings;
-}
-
-add_filter( 'booked_fe_appt_form_bookings', 'online_vacation_booked_fe_appt_form_bookings', 10, 1 );
-
-
-function add_appt_form_date_time_after( $value, $this_timeslot_timestamp, $timeslot, $calendar_id ) {
-    my_log('add_appt_form_date_time_after');
-    $details = get_option('booked_defaults_'.$calendar_id);
-    //my_log($details);
-    my_log(compact('value', 'this_timeslot_timestamp', 'timeslot', 'calendar_id'));
-    
-}
-
-add_filter( 'booked_fe_appt_form_date_time_before', 'add_appt_form_date_time_after', 10, 4);
-
-/*
-add_filter( 'booked_custom_fields_add_template_main', 'test', 10, 8 );
-
-function test_two( $field_type, $field_name, $field_value, $is_required, $look_for_subs, $numbers_only, $data_attributes ) {
-    my_log('test two');
-    my_log( compact('field_type', 'field_name', 'field_value', 'is_required', 'look_for_subs', 'numbers_only', 'data_attributes') );
-}
-
-add_filter( 'booked_custom_fields_add_template_subs', 'test_two', 10, 7 );
-*/
-function test_three( $field_type, $look_for_subs ) {
-    my_log('test three');
-    my_log( compact('field_type', 'look_for_subs') );
-}
-
-add_action( 'booked_custom_fields_add_template_subs_end', 'test_three' );
-
+add_action('wp_ajax_booked_online_vacation_get', 'booked_online_vacation_get');
+add_action('wp_ajax_nopriv_booked_online_vacation_get', 'booked_online_vacation_get');
--- js/booked-online-front.js
+++ js/booked-online-front.js
@@ -1,6 +1,25 @@
 window.addEventListener('load', (event) => {
-    console.log("It works!");
-    document.addEventListener('booked-on-new-app', function(){
-        console.log("I want this to appear after the modal has opened!");
+    jQuery(document).on("booked-on-new-app", function (event) {
+        const $details = jQuery('.booked-appointment-details');
+        let timeslot = $details.find('input[name="timeslot[]"]').val();
+        let date = $details.find('input[name="date[]"]').val();
+        let calendar_id = $details.find('input[name="calendar_id[]"]').val();
+        booked_appt_form_options = { 'action': 'booked_online_vacation_get', 'nonce': booked_js_vars.nonce, 'date': date, 'timeslot': timeslot, 'calendar_id': calendar_id };
+
+        jQuery.ajax({
+            url: booked_js_vars.ajax_url,
+            type: 'post',
+            data: booked_appt_form_options,
+            success: function (response) {
+                if (response.success && response.data.onlineVacationCheckbox) {
+                    const $container = jQuery('.cf-block');
+                    $container.find('input[value="Élőben veszek részt"]').parent().hide();
+                    $container.find('input[value="Utólag megnézem"]').prop('checked', true);
+                }
+            },
+            error: function (error) {
+                // console.log(error);
+            }
+        });
     });
 })
